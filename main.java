7c21fb70b4d15b6dc2dacd993eccfd2b94ebb187
Kiash254 <codewithkiash@gmail.com>
2023-02-23 17:25:26 +0300

    addded java code

--- main.java
+++ main.java
@@ -1,6 +1,22 @@
 // write hello world in java
 public class main {
     public static void main(String[] args) {
+        //using println
         System.out.println("Hello World");
+        System.out.println("Am a Newbie in Java");
+        System.out.println("I love Java");
+        //using print
+        System.out.print("Hello World");
+        System.out.print("Am a Newbie in Java");
+        System.out.print("I love Java");
+        //its kiash in java from python programmer write this code
+        System.out.println("Hello World kiash is a new programmer in java");
+
+        //print number using println and print
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
     }
 }
